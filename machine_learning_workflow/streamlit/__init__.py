8c75851fc2a5c8dbec2b6cc2676516b5d9ab12cc
project-vending <131410219+project-vending@users.noreply.github.com>
2023-04-28 10:24:36 -0400

    Add __init__.py

---
+++ machine_learning_workflow/streamlit/__init__.py
@@ -0,0 +1,18 @@
+python
+# __init__.py
+
+# Add any required import statements here, like:
+
+# import streamlit as st
+
+# Define any required variables here, like:
+
+# MODEL_FILE = 'model.pkl'
+
+# Define any required functions here, like:
+
+# def load_model():
+#     model = joblib.load(MODEL_FILE)
+#     return model
+
+# You can add any other code here that needs to be executed when the package is imported.
